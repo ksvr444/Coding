c7851fbc3aaabfded53b7d90178d29efb254086b
ksvr444 <ksvr444@users.github.com>
2019-07-14 08:21:40 +0530

    modified join function

--- largestPalindrome.py
+++ largestPalindrome.py
@@ -5,7 +5,7 @@ for i in range(len(s)):
     for j in range(i+2, len(s)+1):
         temp = s[i : j] m
         lgth = len(temp)
-        if temp == ''.join(map(str, reversed(temp))) and max_lgth < lgth:
+        if temp == ''.join(reversed(temp)) and max_lgth < lgth:
             max_pal = temp
             max_lgth = lgth
 
