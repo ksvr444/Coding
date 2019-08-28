1d0624491265267d1e1edc59654fde5c7ce6b79d
ksvr444 <31118012+ksvr444@users.noreply.github.com>
2019-08-28 19:24:05 +0530

    Update checkBST.py

--- checkBST.py
+++ checkBST.py
@@ -1,8 +1,8 @@
 def checkBST(root, min_val, max_val):
     if not root:
         return True
-    if root.data > min_val || root.data < max_val:
+    if root.data > min_val or root.data < max_val:
         return False
-    return checkBST(root.left, min_val, root.data-1) && checkBST(root.right, root.data+1, max_val)
+    return checkBST(root.left, min_val, root.data-1) and checkBST(root.right, root.data+1, max_val)
   
   
